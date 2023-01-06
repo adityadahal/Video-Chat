e450e50d5dc2cd5f9a8e159226e3c4c157ebab77
Aditya Dahal <toadityadahal@gmail.com>
2023-01-06 12:54:32 +0545

    settings option removed from Client

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientComponent } from './client/client.component';
 import { MainComponent } from './pages/main/main.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { RoomComponent } from './pages/room/room.component';
 
-const routes: Routes = [{ path: '', component: RoomComponent }];
+const routes: Routes = [
+  { path: '', component: RoomComponent },
+  { path: 'client', component: ClientComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: false })],
